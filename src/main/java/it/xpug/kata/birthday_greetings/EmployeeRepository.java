35919786da86c14f422f86d9fb73203f497b2049
aalberti <aa@antoinealberti.com>
2022-12-05 16:13:08 +0100

    Remove all code

--- src/main/java/it/xpug/kata/birthday_greetings/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package it.xpug.kata.birthday_greetings;
-
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.List;
-
-public interface EmployeeRepository {
-    List<Employee> getAll() throws IOException, ParseException;
-}
