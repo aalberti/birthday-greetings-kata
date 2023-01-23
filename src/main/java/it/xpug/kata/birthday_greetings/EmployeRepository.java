a8a23f5e3353729b5d4f03c12b64e1da0ff36a2d
hbentalha <adm-azr-hicham.bentalha@chanel.com>
2023-01-23 12:26:44 +0100

    create interface IEmployeRepository

--- src/main/java/it/xpug/kata/birthday_greetings/EmployeRepository.java
+++ src/main/java/it/xpug/kata/birthday_greetings/EmployeRepository.java
@@ -1,28 +1,9 @@
 package it.xpug.kata.birthday_greetings;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeRepository {
-     private final String fileName;
-
-    public EmployeRepository(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public List<Employee> getEmploye() throws IOException, ParseException {
-        List<Employee> employees = new ArrayList<>();
-        BufferedReader in = new BufferedReader(new FileReader(this.fileName));
-        String str = "";
-        str = in.readLine(); // skip header
-        while ((str = in.readLine()) != null) {
-            String[] employeeData = str.split(", ");
-            employees.add(new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]));
-        }
-        return employees;
-    }
+public interface EmployeRepository {
+    List<Employee> getEmploye() throws IOException, ParseException;
 }
--- src/main/java/it/xpug/kata/birthday_greetings/Main.java
+++ src/main/java/it/xpug/kata/birthday_greetings/Main.java
@@ -9,7 +9,7 @@ import javax.mail.internet.*;
 public class Main {
 
 	public static void main(String[] args) throws AddressException, IOException, ParseException, MessagingException {
-		BirthdayService service = new BirthdayService(new EmailSender("localhost", 25), new EmployeRepository("employee_data.txt"));
+		BirthdayService service = new BirthdayService(new EmailSender("localhost", 25), new IEmployeRepository("employee_data.txt"));
 		service.sendGreetings(new XDate());
 	}
 
