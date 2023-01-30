19563850c2c3cbceeef2c3d5c6806ed91bcc729e
aalberti <aa@antoinealberti.com>
2023-01-30 12:00:18 +0100

    Move sendMessage to MailSender

--- src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
+++ src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
@@ -1,11 +1,6 @@
 package it.xpug.kata.birthday_greetings;
 
-import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -29,26 +24,8 @@ public class BirthdayService {
 				String recipient = employee.getEmail();
 				String body = "Happy Birthday, dear %NAME%!".replace("%NAME%", employee.getFirstName());
 				String subject = "Happy Birthday!";
-				sendMessage(smtpHost, smtpPort, "sender@here.com", subject, body, recipient);
+				mailSender.sendMessage(smtpHost, smtpPort, "sender@here.com", subject, body, recipient);
 			}
 		}
-	}
-
-	private void sendMessage(String smtpHost, int smtpPort, String sender, String subject, String body, String recipient) throws MessagingException {
-		// Create a mail session
-		java.util.Properties props = new java.util.Properties();
-		props.put("mail.smtp.host", smtpHost);
-		props.put("mail.smtp.port", "" + smtpPort);
-		Session session = Session.getInstance(props, null);
-
-		// Construct the message
-		Message msg = new MimeMessage(session);
-		msg.setFrom(new InternetAddress(sender));
-		msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-		msg.setSubject(subject);
-		msg.setText(body);
-
-		// Send the message
-		Transport.send(msg);
 	}
 }
